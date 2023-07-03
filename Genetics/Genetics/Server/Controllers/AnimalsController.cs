273d25ac32b37c944d03370b9432133d0b06578d
jsepulveda <132211911+josesepulveda-v@users.noreply.github.com>
2023-07-02 22:48:08 -0500

    GetAnimal
    GetAnimal in front

---
+++ Genetics/Genetics/Server/Controllers/AnimalsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Genetics.Server.Dal;
+using Genetics.Shared.Models;
+
+namespace Genetics.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly AnimalContext _context;
+
+        public AnimalsController(AnimalContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAnimal")]
+        public async Task<ActionResult<IEnumerable<Animal>>> GetAnimal()
+        {
+          if (_context.Animal == null)
+          {
+              return NotFound();
+          }
+            return await _context.Animal.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animal>> GetAnimal(int id)
+        {
+          if (_context.Animal == null)
+          {
+              return NotFound();
+          }
+            var animal = await _context.Animal.FindAsync(id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return animal;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAnimal(int id, Animal animal)
+        {
+            if (id != animal.AnimalId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(animal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost("AddAnimal")]
+        public async Task<ActionResult<Animal>> AddAnimal(Animal animal)
+        {
+          if (_context.Animal == null)
+          {
+              return Problem("Entity set 'AnimalContext.Animal'  is null.");
+          }
+            _context.Animal.Add(animal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAnimal", new { id = animal.AnimalId }, animal);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal(int id)
+        {
+            if (_context.Animal == null)
+            {
+                return NotFound();
+            }
+            var animal = await _context.Animal.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Animal.Remove(animal);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AnimalExists(int id)
+        {
+            return (_context.Animal?.Any(e => e.AnimalId == id)).GetValueOrDefault();
+        }
+    }
+}
